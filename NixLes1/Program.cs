3a95a081c16a93c90dbef81bc3f2623598169923
timass <salyga.timofej@gmail.com>
2021-07-14 14:19:09 +0300

    Add project files.

---
+++ NixLes1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NixLes1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Human!");
+        }
+        class Human
+        { 
+        public string Name { get; set; }
+        public int Age { get; set; }
+            public void Print(string str, int ag)
+            {
+                Console.WriteLine($"Name: {str}  Age: {ag}");
+            }
+        }
+
+    }
+}
